0d47648473f40f8492c4c19083eaa50519a05edd
Tarun Kumar Sao <tarun.k.sao@gmail.com>
2023-04-12 16:55:13 +0530

    Added Default Umbrella display Image and animation to loading image

--- Scripts/index.js
+++ Scripts/index.js
@@ -4,18 +4,23 @@ const radio1 = document.getElementById('radio1');
 const radio2 = document.getElementById('radio2');
 const radio3 = document.getElementById('radio3');
 
-radio1.addEventListener('click', function () {
+radio1.addEventListener('select', function () {
     color = 'pink';
+    console.log(color);
     customize(color);
 });
 
-radio2.addEventListener('click', function () {
+radio2.addEventListener('select', function () {
     color = 'blue';
+    console.log(color);
+
     customize(color);
 });
 
-radio3.addEventListener('click', function () {
+radio3.addEventListener('select', function () {
     color = 'yellow';
+    console.log(color);
+
     customize(color);
 });
 
@@ -25,9 +30,11 @@ function customize(color) {
     setTimeout(() => {
         const img = document.createElement('img');
         img.src = `./Images/${color}_umbrella.png`;
+        img.setAttribute('class', 'image');
         umbrella.innerHTML = '';
         umbrella.append(img);
     }, 5000);
+    
     const loadImg = document.createElement('img');
     loadImg.setAttribute('class', 'loadImg');
     loadImg.src = './Images/loader_icon.svg';
